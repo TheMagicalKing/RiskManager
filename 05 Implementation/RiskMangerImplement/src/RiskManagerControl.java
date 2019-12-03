2c5ec93f34b4e663bc7c1c3beb50a7b1faab1f4c
Nicki Hvolgaard <38239769+TheMagicalKing@users.noreply.github.com>
2019-12-03 09:43:12 +0100

    Added Variables and methods to RiskManagerControl

--- 05 Implementation/RiskMangerImplement/src/RiskManagerControl.java
+++ 05 Implementation/RiskMangerImplement/src/RiskManagerControl.java
@@ -1,7 +1,16 @@
 public class RiskManagerControl {
 
+    private RiskTable rt;
+
     public static void main(String[] args) {
         System.out.println("Hej");
+    }
+
+    public String addRisk(String Risk){
+        return Risk;
+    }
+    public void sendRisk(Risk, String description,double probability,double consequence){
+
     }
 
 }
