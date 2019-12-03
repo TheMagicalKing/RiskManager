44a61c5a37fb9d7d28074b0babfc1bbdc32cd899
Nicki Hvolgaard <38239769+TheMagicalKing@users.noreply.github.com>
2019-12-03 11:02:29 +0100

    pushed after making methods in RMC and RiskTable

--- 05 Implementation/RiskMangerImplement/src/RiskTable.java
+++ 05 Implementation/RiskMangerImplement/src/RiskTable.java
@@ -4,5 +4,14 @@ public class RiskTable{
     private ArrayList<Risk> risks = new ArrayList<>();
 
     public Risk addRisk(){return null;}
-    public void sendRisk(Risk risk, String description, double probability, double consequence){}
+    public void sendRisk(Risk risk, String description, double probability, double consequence){
+        if (risks.contains(risk)){
+
+            System.out.println("No Risk Found");
+
+        }
+        else{
+            risk.sendRisk(description, probability,consequence);
+        }
+    }
 }
