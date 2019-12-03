9b92c607092427d74b9b477ec9d966cc01100468
simo215d <simo215d@edu.easj.dk>
2019-12-03 09:55:12 +0100

    test commit

--- 05 Implementation/RiskMangerImplement/src/RiskTable.java
+++ 05 Implementation/RiskMangerImplement/src/RiskTable.java
@@ -2,6 +2,7 @@ import java.util.ArrayList;
 
 public class RiskTable{
     private ArrayList<Risk> risks = new ArrayList<>();
+    private int xd = 0;
 
     public Risk addRisk(){return null;}
     public void sendRisk(Risk risk, String description, double probability, double consequence){}
