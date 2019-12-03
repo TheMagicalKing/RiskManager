48a53dd1caadf129ab1f2a43976d828ab732a8ec
Nicki Hvolgaard <38239769+TheMagicalKing@users.noreply.github.com>
2019-12-03 11:19:31 +0100

    Finished Risk class for now

--- 05 Implementation/RiskMangerImplement/src/Risk.java
+++ 05 Implementation/RiskMangerImplement/src/Risk.java
@@ -1,3 +1,5 @@
+
+
 public class Risk {
    private String description;
    private double probability;
@@ -5,8 +7,15 @@ public class Risk {
 
    public void  sendRisk( String description, double probability, double consequence) {
 
-
+       if (probability<0||consequence<0){
+           System.out.println("Probability or Consequense is less than zero!");
+       }else {
+           System.out.println("Risk Updated!");
+           this.description = description;
+           this.probability = probability;
+           this.consequence = consequence;
+       }
    }
-    }
+}
 
 
