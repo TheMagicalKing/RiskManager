b904dae1d7a8386367497e1042b38b0144adc45a
Nicki Hvolgaard <38239769+TheMagicalKing@users.noreply.github.com>
2019-12-04 14:31:35 +0100

    Made DB_Con, just insert your Password into the empty string, and remember to add DB_Con to .gitignore file afterwards

---
+++ 05 Implementation/RiskMangerImplement/src/DATABASE/DB_Con.java
@@ -0,0 +1,11 @@
+package DATABASE;
+
+public class DB_Con {
+    public static String getPassword(){
+        return "";
+    }
+
+    public static String geUsername(){
+        return "root";
+    }
+}
