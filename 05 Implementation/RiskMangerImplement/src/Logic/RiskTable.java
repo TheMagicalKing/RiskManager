869d08b28d9275019ca7bd07f191d7e5c8dde075
simo215d <simo215d@edu.easj.dk>
2019-12-05 10:45:20 +0100

    we can now delte risks

--- 05 Implementation/RiskMangerImplement/src/Logic/RiskTable.java
+++ 05 Implementation/RiskMangerImplement/src/Logic/RiskTable.java
@@ -6,14 +6,13 @@ public class RiskTable{
     private ArrayList<Risk> risks = new ArrayList<>();
 
     public Risk addRisk(){
-        //TODO this should tell the ui to add this risk to its observable list, as well as send the new risk to database
-
         Risk risk = new Risk();
         risks.add(risk);
         return null;
     }
     public void sendRisk(Risk risk, String description, double probability, double consequence){
         if (risks.contains(risk)){
+
             risk.sendRisk(description, probability,consequence);
         }
         else{
