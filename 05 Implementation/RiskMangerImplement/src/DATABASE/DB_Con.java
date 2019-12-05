4bbb8f85b3515b652989b834d199bb8cc71732bb
Nicki Hvolgaard <38239769+TheMagicalKing@users.noreply.github.com>
2019-12-05 13:53:40 +0100

    Update DB_Con.java

--- 05 Implementation/RiskMangerImplement/src/DATABASE/DB_Con.java
+++ 05 Implementation/RiskMangerImplement/src/DATABASE/DB_Con.java
@@ -7,13 +7,13 @@ public class DB_Con {
     private static String url = "jdbc:mysql://localhost/riskmanager?ServerTimezone=UTC";
     private static String driverName = "com.mysql.jdbc.Driver";
     private static String username = "root";
-    private static String password = "";
+    private static String password = "1";
     private static Connection con;
 
 
     public static Connection getConnection() {
             try {
-                con = DriverManager.getConnection("jdbc:mysql://localhost/riskmanager?ServerTimezone=UTC", "root", "");
+                con = DriverManager.getConnection("jdbc:mysql://localhost/riskmanager?ServerTimezone=UTC", "root", "1");
             } catch (SQLException ex) {
                 // log an exception. fro example:
                 System.out.println("Failed to create the database connection.");
